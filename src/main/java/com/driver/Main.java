7159cf074f2a64cbd73cba198ee4c9e80f96009f
chiranjeevi3398 <gchiranjeevi3398@gmail.com>
2023-03-14 12:38:27 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      //creating object for RWOnly class
+      RWOnly object=new RWOnly();
+
+      object.setName("chiranjeevi");
+
+
+      System.out.println(object.getName());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    //initializing a private String variable
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
 }
